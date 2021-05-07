f739333b5a2f5b87db7051d1c9428c83ed9c06ef
Nathan Sadler <nssadler@my.waketech.edu>
2021-05-07 19:17:44 -0400

    Refactor scrabble project to use inheritance

--- Tuesday/scrabble_score/lib/scrabble_score.rb
+++ Tuesday/scrabble_score/lib/scrabble_score.rb
@@ -1,20 +1,41 @@
-class String
-  def scrabble
-    # Creates the scoring system
-    score_system = Hash.new()
-    score_system_helper(score_system, [:a, :e, :i, :o, :u, :l, :n, :r, :s, :t], 1)
-    score_system_helper(score_system, [:d, :g], 2)
-    score_system_helper(score_system, [:b, :c, :m, :p], 3)
-    score_system_helper(score_system, [:f, :h, :v, :w, :y], 4)
-    score_system.store("K", 5)
-    score_system_helper(score_system, [:j, :x], 8)
-    score_system_helper(score_system, [:q, :z], 10)
+class Word < String
 
+  def initialize(word)
+    super(word)
+    # Creates the scoring system
+    @score_system = Hash.new()
+    score_system_helper(@score_system, [:a, :e, :i, :o, :u, :l, :n, :r, :s, :t], 1)
+    score_system_helper(@score_system, [:d, :g], 2)
+    score_system_helper(@score_system, [:b, :c, :m, :p], 3)
+    score_system_helper(@score_system, [:f, :h, :v, :w, :y], 4)
+    @score_system.store("K", 5)
+    score_system_helper(@score_system, [:j, :x], 8)
+    score_system_helper(@score_system, [:q, :z], 10)
+  end
+
+  def scrabble(word=self)
     score_total = 0
     self.upcase.split('').each do |letter|
-      score_total += score_system.fetch(letter)
+      score_total += get_letter_score(letter)
     end
     score_total
+  end
+
+  def get_letter_score(letter)
+    return @score_system.fetch(letter)
+  end
+
+end
+
+class Sentence < Word
+  def initialize(word)
+    super(word)
+  end
+
+  def scrabble
+    score_total = 0
+    nospace = self.upcase.split('').select {|n| n!=" "}
+    nospace.reduce(0) {|total, n| total += get_letter_score(n)}
   end
 end
 
--- Tuesday/scrabble_score/spec/scrabble_score_spec.rb
+++ Tuesday/scrabble_score/spec/scrabble_score_spec.rb
@@ -1,13 +1,18 @@
 require('rspec')
 require('scrabble_score')
 
-describe('#scrabble') do
+describe('Word#scrabble') do
   it("returns a scrabble score for a letter") do
-    expect("a".scrabble()).to(eq(1))
+    expect(Word.new("a").scrabble).to(eq(1))
   end
 
   it("returns the sum of each letter's score for a word") do
-    expect("zinc".scrabble).to(eq(15))
+    expect(Word.new("zinc").scrabble).to(eq(15))
   end
+end
 
+describe('Sentence#scrabble') do
+  it("returns the sum of each word's scrabble score") do
+    expect(Sentence.new("Hello World").scrabble).to(eq(17))
+  end
 end
